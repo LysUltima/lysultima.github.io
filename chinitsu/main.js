bc384b5b445fb936bb83a401a444af3a767988a3
LysUltima <68317182+LysUltima@users.noreply.github.com>
2022-09-29 18:54:45 -0400

    Add files via upload

---
+++ chinitsu/main.js
@@ -0,0 +1,54 @@
+var color = "Regular";
+var suit;
+var hand;
+function createHand(value) {
+	//hand
+	hand = new Array(14);
+	//iteration variables
+	var i;
+	var c;
+	
+	var validHand = false;
+	while (!validHand) {
+		for (i = 0; i < hand.length; i++) {
+			hand[i] = Math.floor(Math.random() * 9) + 1;
+		}
+		hand.sort();
+		validHand = true;
+		c = 1;
+		for (i = 1; i < hand.length; i++) {
+			if (hand[i-1] == hand[i]) {
+				c++;
+			}
+			else {
+				c = 1;
+			}
+			if (c > 4) {
+				validHand = false;
+			}
+		}
+	}
+	//random suit
+	suit = Math.floor(Math.random() * 3);
+	suit = suit == 0 ? "Man" : suit == 1 ? "Pin" : "Sou";
+	
+	drawHand();
+}
+function drawHand() {
+	var shorthand = ""; 
+	//iteration variable
+	var i;
+	for (i = 0; i < hand.length; i++) {
+		shorthand += String(hand[i]);
+		document.getElementById("tile" + i).innerHTML = "<img class=front src=\"" + color + "/Front.svg\"><img class=tilepaint src=\"" + color + "/" + suit + hand[i] + ".svg\">";
+	}
+	
+	//add link to tenhou
+	shorthand += suit == "Man" ? "m" : suit == "Pin" ? "p" : "s";
+	document.getElementById("answer").setAttribute("onclick","window.open('https://tenhou.net/2/?q=" + shorthand + "');");
+}
+function switchColor() {
+	color = color == "Regular" ? "Black" : "Regular";
+	drawHand();
+}
+createHand();
